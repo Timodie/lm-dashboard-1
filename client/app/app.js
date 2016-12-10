fb3246549ed118fa1ac8aa1fa4e53b73f2a0f237
loganrennick <loganrennick@gmail.com>
2016-12-10 17:03:09 -0500

    fixed 'main' react component and added route

--- client/app/app.js
+++ client/app/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Macro_Status_Page from './Components/Macro_Status_Dev_page.js';
 import Peer_Review_Page from './Components/Peer_Review_Adm_page.js';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
+import MainPanel from './Components/main.js';
 
 
 class macro_status_page extends React.Component {
@@ -25,6 +26,17 @@ class peer_review_page extends React.Component {
   }
 }
 
+class main_page extends React.Component {
+  render() {
+    return (
+      <div>
+        <MainPanel />
+      </div>
+    );
+  }
+}
+
+
 
 class App extends React.Component {
   render() {
@@ -40,7 +52,8 @@ ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       {/* Show the Feed at / */}
-      <IndexRoute component={macro_status_page} />
+      <IndexRoute component={main_page} />
+      <Route path="Main" component={main_page} />
       <Route path="Macro_Status" component={macro_status_page} />
       <Route path="Peer_Review" component={peer_review_page} />
     </Route>
