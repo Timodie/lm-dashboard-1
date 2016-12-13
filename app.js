f7e3d7bb0f9d2da1986c24c4952ecda15104f42b
Thyrfa <thyrfa@gmail.com>
2016-12-13 15:30:59 -0500

    Passport strategy

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ passport.use(new Strategy(
     db.users.findByUsername(username, function(err, user) {
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
+      if (user.password != password) { return done(null, false); }
       return done(null, user);
     });
   }));
