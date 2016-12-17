e0c17dd8c09086b8087343bbd1a5aa050d5a29ea
Zitao Wang <mrwongs86@gmail.com>
2016-12-16 19:48:33 -0500

    remove writer.py and credential

--- Database Stuff/mysql_runner.py
+++ Database Stuff/mysql_runner.py
@@ -2,10 +2,10 @@
 import pymysql
 import sys
 
-hostname = '138.197.32.168'
-username = 'Zitao'
-password = '!Boston1994'
-database = 'CS320'
+hostname = 'hostname'
+username = 'username'
+password = 'password'
+database = 'database'
 
 # Driver function to connect DB.
 myConnection = pymysql.connect( host=hostname, user=username, passwd=password, db=database )
