ce037376240a72f45d06e1e7631db3dd815dd7fd
jizhongce <jizhongce123@gmail.com>
2016-12-16 19:24:03 -0500

    Add some dependencies in the package.json, and reconginaze file and also add express static to server the web so use node and local 3000 to show the website

---
+++ server/app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('./db');
+
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.user_id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+var app = express();
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('../client/build'));
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
