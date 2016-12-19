b8173eb04ec2cd3f66b2ef9ea3354539d3a4f3f4
timodie <timodie.addai@gmail.com>
2016-12-19 17:34:38 -0500

    few ui changes

--- client/app/components/ViewLog.js
+++ client/app/components/ViewLog.js
@@ -23,8 +23,8 @@ render(){
           </tr>
         </thead>
         <tbody>
-          <View_log_Entry date={1452668480000} status="Aproved" username="tim" macroname="View Car Tables" finishtime={1453668480000} data="This is my data"/>
-          <View_log_Entry date={1453668480000} status="Aproved" username="Logan Rennick" macroname="Update Life Policy" finishtime={1472668480000} data="This is my data"/>
+          <View_log_Entry date={1452668480000} status="Approved" username="tim" macroname="View Car Tables" finishtime={1453668480000} data="This is my data"/>
+          <View_log_Entry date={1453668480000} status="Approved" username="Logan Rennick" macroname="Update Life Policy" finishtime={1472668480000} data="This is my data"/>
           <View_log_Entry date={1472668480000} status="Rejected" username="Tim Addai" macroname="Delete Life Policy" finishtime={1482668480000} data=""/>
           <View_log_Entry date={1482668480000} status="Pending" username="Eric Gendreau" macroname="Update Housing Policy" finishtime={0} data=""/>
           <View_log_Entry date={1492668480000} status="Approved" username="Jason Lee" macroname="Add New Housing Table" finishtime={0} data=""/>
